082db134a94bbdf3d295861ffa708b78fc533ca1
Samuel Joao Barbosa Pinto <samuel_jbp@yahoo.com.br>
2024-07-02 11:13:31 -0300

    Reorganizando os arquivos

--- run.py
+++ run.py
@@ -1,138 +1,95 @@
-import pygame
-from pygame.locals import *
+"""O problema da mochila: um problema de otimização combinatória.
+O nome dá-se devido ao modelo de uma situação em que é necessário
+preencher uma mochila com objetos de diferentes pesos e valores.
+O objetivo é que se preencha a mochila com o maior valor possível,
+não ultrapassando o peso máximo."""
+
+from gen_alg import *
+from draw_methods import *
 import random
-import itertools
-from genetic_algorithm import mutate, order_crossover, generate_random_population, calculate_fitness, sort_population, default_problems
-from draw_functions import draw_paths, draw_plot, draw_cities
-import sys
-import numpy as np
-import pygame
-from benchmark_att48 import *
 
+# [peso,valor]
+# itens_disponiveis = [[4, 30], [8, 10], [8, 30], [25, 75],
+#                    [2, 10], [50, 100], [6, 300], [12, 50],
+#                    [100, 400], [8, 300]]
+# gera o conjunto de itens de forma aleatória. Gera 20 itens com peso entre 1 e 10 e valor entre 1 e 300
+itens_disponiveis = [(random.randint(1, 10), random.randint(1, 300)) for _ in range(20)]
 
-# Define constant values
-# pygame
-WIDTH, HEIGHT = 800, 400
-NODE_RADIUS = 10
-FPS = 30
-PLOT_X_OFFSET = 450
+peso_maximo = 100
+tamanho_populacao = 150
+max_geracoes = 200
+qtd_itens_disponiveis = len(itens_disponiveis)
 
-# GA
-N_CITIES = 15
-POPULATION_SIZE = 100
-N_GENERATIONS = None
-MUTATION_PROBABILITY = 0.5
-
-# Define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-
-
-# Initialize problem
-# Using Random cities generation
-cities_locations = [(random.randint(NODE_RADIUS + PLOT_X_OFFSET, WIDTH - NODE_RADIUS), random.randint(NODE_RADIUS, HEIGHT - NODE_RADIUS))
-                    for _ in range(N_CITIES)]
-
-
-# # Using Deault Problems: 10, 12 or 15
-# WIDTH, HEIGHT = 800, 400
-# cities_locations = default_problems[15]
-
-
-# Using att48 benchmark
-# WIDTH, HEIGHT = 1500, 800
-# att_cities_locations = np.array(att_48_cities_locations)
-# max_x = max(point[0] for point in att_cities_locations)
-# max_y = max(point[1] for point in att_cities_locations)
-# scale_x = (WIDTH - PLOT_X_OFFSET - NODE_RADIUS) / max_x
-# scale_y = HEIGHT / max_y
-# cities_locations = [(int(point[0] * scale_x + PLOT_X_OFFSET),
-#                      int(point[1] * scale_y)) for point in att_cities_locations]
-# target_solution = [cities_locations[i-1] for i in att_48_cities_order]
-# fitness_target_solution = calculate_fitness(target_solution)
-# print(f"Best Solution: {fitness_target_solution}")
-# ----- Using att48 benchmark
-
-
-# Initialize Pygame
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("TSP Solver using Pygame")
-clock = pygame.time.Clock()
-generation_counter = itertools.count(start=1)  # Start the counter at 1
-
-
-# Create Initial Population
-# TODO:- use some heuristic like Nearest Neighbour our Convex Hull to initialize
-population = generate_random_population(cities_locations, POPULATION_SIZE)
-best_fitness_values = []
-best_solutions = []
-
-
-# Main game loop
 running = True
+cont_geracao = 0
+
+populacao = population(tamanho_populacao, qtd_itens_disponiveis)
+historico_de_fitness = [media_fitness(populacao, peso_maximo, itens_disponiveis)]
+
+mutate = 0.05
+
+
+def inverter_array(array):
+    return array[::-1]
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                running = False
+                running = False  # Quit the game when the 'q' key is pressed
 
-    generation = next(generation_counter)
+    if cont_geracao < max_geracoes:
 
-    screen.fill(WHITE)
+        pais = [[fitness(x, peso_maximo, itens_disponiveis), x] for x in populacao if
+                fitness(x, peso_maximo, itens_disponiveis) >= 0]
+        pais.sort(reverse=True)
 
-    population_fitness = [calculate_fitness(
-        individual) for individual in population]
+        # REPRODUCAO
+        filhos = []
+        while len(filhos) < tamanho_populacao:
+            homem, mulher = selecao_roleta(pais)
+            meio = len(homem) // 2
+            filho = homem[:meio] + mulher[meio:]
+            filhos.append(filho)
 
-    population, population_fitness = sort_population(
-        population,  population_fitness)
+        # MUTACAO
+        for individuo in filhos:
+            if mutate > random.random():
+                pos_to_mutate = randint(0, len(individuo) - 1)
+                if individuo[pos_to_mutate] == 1:
+                    individuo[pos_to_mutate] = 0
+                else:
+                    individuo[pos_to_mutate] = 1
 
-    best_fitness = calculate_fitness(population[0])
-    best_solution = population[0]
+        populacao = filhos
 
-    best_fitness_values.append(best_fitness)
-    best_solutions.append(best_solution)
+        historico_de_fitness.append(media_fitness(populacao, peso_maximo, itens_disponiveis))
+        cont_geracao += 1
 
-    draw_plot(screen, list(range(len(best_fitness_values))),
-              best_fitness_values, y_label="Fitness - Distance (pxls)")
+        historico_de_fitness_invertido = inverter_array(historico_de_fitness)
+        draw_plot(list(range(len(historico_de_fitness))), historico_de_fitness)
 
-    draw_cities(screen, cities_locations, RED, NODE_RADIUS)
-    draw_paths(screen, best_solution, BLUE, width=3)
-    draw_paths(screen, population[1], rgb_color=(128, 128, 128), width=1)
+        draw_text(screen, f'Melhor resultado: {"{:.2f}".format(max(historico_de_fitness))}',
+                  930, window_size[1] - 100, font_size=25, font='Arial')
 
-    print(f"Generation {generation}: Best fitness = {round(best_fitness, 2)}")
+        tick_clock()
+    else:
+        running = False
+        #quit_pygame()
 
-    new_population = [population[0]]  # Keep the best individual: ELITISM
+# PRINTS DO TERMINAL
+for indice, dados in enumerate(historico_de_fitness):
+    print("Geracao: ", indice, " | Media de valor na mochila: ", dados)
 
-    while len(new_population) < POPULATION_SIZE:
+print("\nPeso máximo:", peso_maximo, "g\n\nItens disponíveis:")
+for indice, i in enumerate(itens_disponiveis):
+    print("Item ", indice + 1, ": ", i[0], "g | R$", i[1])
 
-        # selection
-        # simple selection based on first 10 best solutions
-        # parent1, parent2 = random.choices(population[:10], k=2)
+print("\nExemplos de boas solucoes: ")
+for i in range(5):
+    print(populacao[i])
 
-        # solution based on fitness probability
-        probability = 1 / np.array(population_fitness)
-        parent1, parent2 = random.choices(population, weights=probability, k=2)
-
-        # child1 = order_crossover(parent1, parent2)
-        child1 = order_crossover(parent1, parent1)
-
-        child1 = mutate(child1, MUTATION_PROBABILITY)
-
-        new_population.append(child1)
-
-    population = new_population
-
-    pygame.display.flip()
-    clock.tick(FPS)
-
-
-# TODO: save the best individual in a file if it is better than the one saved.
-
-# exit software
-pygame.quit()
-sys.exit()
+k = input("press close to exit")
